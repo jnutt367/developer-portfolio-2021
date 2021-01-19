e67aff61a5860f7566c4e74f8cef7d8b5f85fe64
Jason Nutt <jnutt367@gmail.com>
2021-01-19 17:39:09 -0500

    Committing often..
    added shooter, fixed up grid, changed some styles. It's coming along...Thanks Ania Kubow!!!

---
+++ js/invaders.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div')
+    const resultDisplay = document.querySelector('#result')
+    let width = 15
+    let currentShooterIndex = 202
+    let currentInvaderIndex = 0
+    let alienInvadersTakenDown = []
+    let result = 0
+    let direction = 1
+    let invaderId
+
+    // define the alien invaders
+    const alienInvaders = [
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+        15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+        30, 31, 32, 33, 34, 35, 36, 37, 38, 39
+    ]
+
+    //draw alien invaders
+    alienInvaders.forEach(invader => squares[currentInvaderIndex + invader].classList.add('invader'))
+
+
+    // draw shooter
+    squares[currentShooterIndex].classList.add('shooter')
+
+    // move the shooter
+
+})
